6811d0b7be58e4cabe39e7a10a62ea2c8a65d07b
JDcamacaro <143712820+JDcamacaro@users.noreply.github.com>
2026-02-09 12:14:09 -0400

    iniciando proyecto rama principal

---
+++ src/main/java/com/sistema_de_gestion_de_prestamos_libros/SistemaDeGestionDePrestamosLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_de_gestion_de_prestamos_libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionDePrestamosLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionDePrestamosLibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema_de_gestion_de_prestamos_libros/SistemaDeGestionDePrestamosLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_de_gestion_de_prestamos_libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestionDePrestamosLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
